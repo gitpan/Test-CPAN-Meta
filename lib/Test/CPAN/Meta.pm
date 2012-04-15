acde415b1aa462b89ce7d45be5b176b624218bb7
Barbie <barbie@missbarbell.co.uk>
2012-04-15 09:03:12 +0000

    Import of BARBIE/Test-CPAN-Meta-0.19 from CPAN.
    gitpan-cpan-distribution: Test-CPAN-Meta
    gitpan-cpan-version:      0.19
    gitpan-cpan-path:         BARBIE/Test-CPAN-Meta-0.19.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Test/CPAN/Meta.pm
+++ lib/Test/CPAN/Meta.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '0.18';
+$VERSION = '0.19';
 
 #----------------------------------------------------------------------------
 
@@ -215,9 +215,9 @@ for Miss Barbell Productions, L<http://www.missbarbell.co.uk>
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (C) 2007-2011 Barbie for Miss Barbell Productions
+  Copyright (C) 2007-2012 Barbie for Miss Barbell Productions
 
   This module is free software; you can redistribute it and/or
-  modify it under the same terms as Perl itself.
+  modify it under the Artistic Licence v2.
 
 =cut
